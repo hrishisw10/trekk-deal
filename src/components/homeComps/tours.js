5f443ec53efabfffd78da27c989f07dd50f3f61b
Hrishikesh <hrishisw10@gmail.com>
2023-04-02 12:19:50 +0530

    Added Tours Type in Tour section

--- src/components/homeComps/tours.js
+++ src/components/homeComps/tours.js
@@ -5,8 +5,11 @@ export default function Tours(props){
     console.log("=====\n",props.tour)
     const treksToRender=props.tour.trip.map((trek,index)=><Trek key={index} trek={trek}/>)
     return (
-        <div className="flex-container Tour">
+        <div className="Tour">
+            <h1>{props.tour.toursType}</h1>
+            <div className="flex-container">
             {treksToRender}
+            </div>
         </div>
     )
 } 
